3ec2f49d61096a16f57d71b3c8e50472d4e450a5
Manoel Aranda Neto <marandaneto@gmail.com>
2024-01-16 10:49:27 +0100

    Flutter macOS support

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import posthog_flutter
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PosthogFlutterPlugin.register(with: registry.registrar(forPlugin: "PosthogFlutterPlugin"))
+}
