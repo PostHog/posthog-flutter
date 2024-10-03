b76e953e750f39c0d13e1cc10e80cf3ab043cfa7
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2024-10-03 09:41:47 +0200

    Update android/src/main/kotlin/com/posthog/posthog_flutter/PosthogFlutterPlugin.kt
    Co-authored-by: Paul D'Ambra <paul.dambra@gmail.com>

--- android/src/main/kotlin/com/posthog/posthog_flutter/PosthogFlutterPlugin.kt
+++ android/src/main/kotlin/com/posthog/posthog_flutter/PosthogFlutterPlugin.kt
@@ -21,7 +21,7 @@ import io.flutter.plugin.common.MethodChannel.Result
 class PosthogFlutterPlugin :
     FlutterPlugin,
     MethodCallHandler {
-    // / The MethodChannel that will the communication between Flutter and native Android
+    // / The MethodChannel that will be the communication between Flutter and native Android
     // /
     // / This local reference serves to register the plugin with the Flutter Engine and unregister it
     // / when the Flutter Engine is detached from the Activity
