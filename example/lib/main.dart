da1ab379e3966589189c711881387d70755fd136
Manoel Aranda Neto <marandaneto@gmail.com>
2025-01-07 11:21:06 +0100

    rollback sample

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,8 +12,8 @@ Future<void> main() async {
   config.captureApplicationLifecycleEvents = false;
   config.host = 'https://us.i.posthog.com';
   config.sessionReplay = true;
-  config.sessionReplayConfig.maskAllTexts = true;
-  config.sessionReplayConfig.maskAllImages = true;
+  config.sessionReplayConfig.maskAllTexts = false;
+  config.sessionReplayConfig.maskAllImages = false;
   config.sessionReplayConfig.throttleDelay = const Duration(milliseconds: 1000);
   config.flushAt = 1;
   await Posthog().setup(config);
