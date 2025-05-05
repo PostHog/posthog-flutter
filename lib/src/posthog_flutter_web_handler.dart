9fa6b2a392dc6e0ad121615aaeb93ac4e6d64f45
Pedro Carvalho <opedrovmc@gmail.com>
2025-05-05 04:14:48 -0300

    Fixed wasm build and improved type safety (#179)

--- lib/src/posthog_flutter_web_handler.dart
+++ lib/src/posthog_flutter_web_handler.dart
@@ -1,107 +1,180 @@
-// ignore: deprecated_member_use
-import 'dart:js';
+import 'dart:js_interop';
 
 import 'package:flutter/services.dart';
 
-Future<dynamic> handleWebMethodCall(MethodCall call, JsObject context) async {
-  final analytics = JsObject.fromBrowserObject(context['posthog']);
+// Definição da interface JS para o PostHog
+@JS()
+@staticInterop
+class PostHog {}
+
+extension PostHogExtension on PostHog {
+  external JSAny? identify(
+      JSAny userId, JSAny properties, JSAny propertiesSetOnce);
+  external JSAny? capture(JSAny eventName, JSAny properties);
+  external JSAny? alias(JSAny alias);
+  external JSAny? get_distinct_id();
+  external void reset();
+  external void debug(JSAny debug);
+  external JSAny? isFeatureEnabled(JSAny key);
+  external void group(JSAny type, JSAny key, JSAny properties);
+  external void reloadFeatureFlags();
+  external void opt_in_capturing();
+  external void opt_out_capturing();
+  external JSAny? getFeatureFlag(JSAny key);
+  external JSAny? getFeatureFlagPayload(JSAny key);
+  external void register(JSAny properties);
+  external void unregister(JSAny key);
+  external JSAny? get_session_id();
+}
+
+// Acessar o posthog a partir do window
+@JS('window.posthog')
+external PostHog? get posthog;
+
+// Funções de conversão
+JSAny stringToJSAny(String value) {
+  return value.toJS;
+}
+
+JSAny boolToJSAny(bool value) {
+  return value.toJS;
+}
+
+JSAny mapToJSAny(Map<dynamic, dynamic> map) {
+  return map.jsify() ?? JSObject();
+}
+
+// Função para converter mapas de forma segura
+Map<String, dynamic> safeMapConversion(dynamic mapData) {
+  if (mapData == null) {
+    return {};
+  }
+
+  if (mapData is Map) {
+    return Map<String, dynamic>.from(
+        mapData.map((key, value) => MapEntry(key.toString(), value)));
+  }
+
+  return {};
+}
+
+Future<dynamic> handleWebMethodCall(MethodCall call) async {
+  final args = call.arguments;
+
   switch (call.method) {
     case 'setup':
       // not supported on Web
-      // analytics.callMethod('setup');
       break;
     case 'identify':
-      final userProperties = call.arguments['userProperties'] ?? {};
+      final userId = args['userId'] as String;
+      final userProperties = safeMapConversion(args['userProperties']);
       final userPropertiesSetOnce =
-          call.arguments['userPropertiesSetOnce'] ?? {};
-      analytics.callMethod('identify', [
-        call.arguments['userId'],
-        JsObject.jsify(userProperties),
-        JsObject.jsify(userPropertiesSetOnce),
-      ]);
+          safeMapConversion(args['userPropertiesSetOnce']);
+
+      posthog?.identify(
+        stringToJSAny(userId),
+        mapToJSAny(userProperties),
+        mapToJSAny(userPropertiesSetOnce),
+      );
       break;
     case 'capture':
-      final properties = call.arguments['properties'] ?? {};
-      analytics.callMethod('capture', [
-        call.arguments['eventName'],
-        JsObject.jsify(properties),
-      ]);
+      final eventName = args['eventName'] as String;
+      final properties = safeMapConversion(args['properties']);
+
+      posthog?.capture(
+        stringToJSAny(eventName),
+        mapToJSAny(properties),
+      );
       break;
     case 'screen':
-      final properties = call.arguments['properties'] ?? {};
-      final screenName = call.arguments['screenName'];
+      final screenName = args['screenName'] as String;
+      final properties = safeMapConversion(args['properties']);
       properties['\$screen_name'] = screenName;
 
-      analytics.callMethod('capture', [
-        '\$screen',
-        JsObject.jsify(properties),
-      ]);
+      posthog?.capture(
+        stringToJSAny('\$screen'),
+        mapToJSAny(properties),
+      );
       break;
     case 'alias':
-      analytics.callMethod('alias', [
-        call.arguments['alias'],
-      ]);
+      final alias = args['alias'] as String;
+
+      posthog?.alias(
+        stringToJSAny(alias),
+      );
       break;
     case 'distinctId':
-      final distinctId = analytics.callMethod('get_distinct_id');
-
-      return distinctId;
+      final distinctId = posthog?.get_distinct_id();
+      return distinctId?.dartify() as String?;
     case 'reset':
-      analytics.callMethod('reset');
+      posthog?.reset();
       break;
     case 'debug':
-      analytics.callMethod('debug', [
-        call.arguments['debug'],
-      ]);
+      final enabled = args['debug'] as bool;
+      posthog?.debug(boolToJSAny(enabled));
       break;
     case 'isFeatureEnabled':
-      final isFeatureEnabled = analytics.callMethod('isFeatureEnabled', [
-            call.arguments['key'],
-          ]) as bool? ??
+      final key = args['key'] as String;
+      final isFeatureEnabled = posthog
+              ?.isFeatureEnabled(
+                stringToJSAny(key),
+              )
+              ?.dartify() as bool? ??
           false;
       return isFeatureEnabled;
     case 'group':
-      analytics.callMethod('group', [
-        call.arguments['groupType'],
-        call.arguments['groupKey'],
-        JsObject.jsify(call.arguments['groupProperties'] ?? {}),
-      ]);
+      final groupType = args['groupType'] as String;
+      final groupKey = args['groupKey'] as String;
+      final groupProperties = safeMapConversion(args['groupProperties']);
+
+      posthog?.group(
+        stringToJSAny(groupType),
+        stringToJSAny(groupKey),
+        mapToJSAny(groupProperties),
+      );
       break;
     case 'reloadFeatureFlags':
-      analytics.callMethod('reloadFeatureFlags');
+      posthog?.reloadFeatureFlags();
       break;
     case 'enable':
-      analytics.callMethod('opt_in_capturing');
+      posthog?.opt_in_capturing();
       break;
     case 'disable':
-      analytics.callMethod('opt_out_capturing');
+      posthog?.opt_out_capturing();
       break;
     case 'getFeatureFlag':
-      final featureFlag = analytics.callMethod('getFeatureFlag', [
-        call.arguments['key'],
-      ]);
-      return featureFlag;
+      final key = args['key'] as String;
+
+      final featureFlag = posthog?.getFeatureFlag(
+        stringToJSAny(key),
+      );
+      return featureFlag?.dartify();
     case 'getFeatureFlagPayload':
-      final featureFlag = analytics.callMethod('getFeatureFlagPayload', [
-        call.arguments['key'],
-      ]);
-      return featureFlag;
+      final key = args['key'] as String;
+
+      final featureFlag = posthog?.getFeatureFlagPayload(
+        stringToJSAny(key),
+      );
+      return featureFlag?.dartify();
     case 'register':
-      final properties = {call.arguments['key']: call.arguments['value']};
-      analytics.callMethod('register', [
-        JsObject.jsify(properties),
-      ]);
+      final key = args['key'] as String;
+      final value = args['value'];
+      final properties = {key: value};
+
+      posthog?.register(
+        mapToJSAny(properties),
+      );
       break;
     case 'unregister':
-      analytics.callMethod('unregister', [
-        call.arguments['key'],
-      ]);
+      final key = args['key'] as String;
+
+      posthog?.unregister(
+        stringToJSAny(key),
+      );
       break;
     case 'getSessionId':
-      final sessionId = analytics.callMethod('get_session_id') as String?;
-
+      final sessionId = posthog?.get_session_id()?.dartify() as String?;
       if (sessionId?.isEmpty == true) return null;
-
       return sessionId;
     case 'flush':
       // not supported on Web
