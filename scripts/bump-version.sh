a50482b8c918202c23a65ade8d16ad33e5b84dfd
Manoel Aranda Neto <marandaneto@gmail.com>
2025-12-10 10:47:29 +0000

    improve bump version

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -18,3 +18,19 @@ perl -pi -e "s/postHogVersion = \".*\"/postHogVersion = \"$NEW_VERSION\"/" andro
 
 # Replace Flutter `version` with the given version
 perl -pi -e "s/^version: .*/version: $NEW_VERSION/" pubspec.yaml
+
+# Update CHANGELOG.md
+echo "Updating CHANGELOG.md..."
+if grep -q "^## Next" CHANGELOG.md; then
+    # Replace "## Next" with the new version
+    perl -pi -e "s/^## Next$/## $NEW_VERSION/" CHANGELOG.md
+    echo "Replaced '## Next' with '## $NEW_VERSION' in CHANGELOG.md"
+else
+    # If "## Next" doesn't exist, add the new version at the top of the file
+    # Create a temporary file with the new content
+    echo "## $NEW_VERSION" > CHANGELOG.md.tmp
+    echo "" >> CHANGELOG.md.tmp
+    cat CHANGELOG.md >> CHANGELOG.md.tmp
+    mv CHANGELOG.md.tmp CHANGELOG.md
+    echo "Added '## $NEW_VERSION' at the top of CHANGELOG.md"
+fi
