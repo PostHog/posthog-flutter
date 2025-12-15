104fd49322158b0e8dca2bbecb3761314ce28d6f
Ioannis J <yiannis@posthog.com>
2025-12-15 10:59:58 +0200

    chore(release): bump to 5.10.0

--- android/src/main/kotlin/com/posthog/flutter/PostHogVersion.kt
+++ android/src/main/kotlin/com/posthog/flutter/PostHogVersion.kt
@@ -1,3 +1,3 @@
 package com.posthog.flutter
 
-internal val postHogVersion = "5.9.1"
+internal val postHogVersion = "5.10.0"
--- ios/Classes/PostHogFlutterVersion.swift
+++ ios/Classes/PostHogFlutterVersion.swift
@@ -8,4 +8,4 @@
 import Foundation
 
 // This property is internal only
-let postHogFlutterVersion = "5.9.1"
+let postHogFlutterVersion = "5.10.0"
