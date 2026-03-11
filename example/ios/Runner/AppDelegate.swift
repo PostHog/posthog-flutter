d60819a9339e2042667c01310533b10881ea72ea
Manoel Aranda Neto <marandaneto@gmail.com>
2026-03-11 14:25:17 +0100

    fix example

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -3,13 +3,6 @@ import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
--- example/macos/Runner/AppDelegate.swift
+++ example/macos/Runner/AppDelegate.swift
@@ -10,4 +10,22 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
+
+  override func applicationDidFinishLaunching(_ notification: Notification) {
+    let controller = mainFlutterWindow?.contentViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(
+      name: "posthog_flutter_example",
+      binaryMessenger: controller.engine.binaryMessenger
+    )
+
+    channel.setMethodCallHandler { (call, result) in
+      if call.method == "triggerNativeCrash" {
+        // Trigger a native crash by index out of range
+        let array: [Int] = []
+        _ = array[99]
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
+  }
 }
